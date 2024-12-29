c94432559c1a4eb742a6f11c56a3c849ec7d090a
Phong <tiphong113@gmail.com>
2024-12-29 16:35:05 +0700

    use lazygit with toogleterm instead of a seperate repo

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -1,21 +1,20 @@
--- nvim v0.8.0
 return {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+  -- Define a custom configuration for LazyGit using toggleterm.nvim
+  "akinsho/toggleterm.nvim", -- Ensure toggleterm.nvim is already installed
+  opts = function()
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      hidden = true,
+      direction = "float", -- Floating terminal window
+    })
+
+    local function toggle_lazygit()
+      lazygit:toggle()
+    end
+
+    -- Set the keymap for LazyGit
+    vim.keymap.set("n", "<leader>gg", toggle_lazygit, { desc = "Open LazyGit" })
+  end,
 }
+
